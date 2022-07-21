9f1859ae2f9fe0d5079a2380d453116e257a6cda
ekbann <ellery@bann.com>
2022-07-21 01:00:46 -0300

    Working on UDIV10, SHR, SHR, ROR, ROL

--- make.sh
+++ make.sh
@@ -4,5 +4,5 @@ name=`echo $1 | cut -f 1 -d '.'`
 prg="${name}.prg"
 set -x	# echo on, and expand variables
 $CC65_BIN/cl65 -o $prg -t cx16 -u __EXEHDR__ -C $LD65_CFG/cx16-asm.cfg $1
-x16 -prg $prg -run
+x16 -prg $prg -run -scale 2
 
